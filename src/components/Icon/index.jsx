be328f9709f0d2b3ed4450ebbd23a8b9048f0480
romagoldobin <goldobinforwork@gmail.com>
2021-08-02 17:53:13 +0500

    added icon component & dialog data

---
+++ src/components/Icon/index.jsx
@@ -0,0 +1,32 @@
+import React from 'react';
+import classNames from 'classnames';
+import PropTypes from 'prop-types';
+import * as icons from '../../icons';
+
+const Icon = ({ size = 20, name, className = '' }) => {
+  const [width, height] = size instanceof Array ? size : [size, size];
+  const [icon, viewBox] = icons[name];
+
+  return (
+    <svg
+      className={classNames('icon', className)}
+      width={width}
+      height={height}
+      dangerouslySetInnerHTML={{ __html: icon }}
+      viewBox={viewBox}
+    />
+  );
+};
+
+Icon.propTypes = {
+  size: PropTypes.number,
+  name: PropTypes.string.isRequired,
+  className: PropTypes.string,
+};
+
+Icon.defaultProps = {
+  size: 20,
+  className: '',
+};
+
+export default Icon;
