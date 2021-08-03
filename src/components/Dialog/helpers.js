462ff980952889bed72cbaa661ef821920d326dc
romagoldobin <goldobinforwork@gmail.com>
2021-08-03 17:30:14 +0500

    a little bit refactoring for normalizeDialog

--- src/components/Dialog/helpers.js
+++ src/components/Dialog/helpers.js
@@ -1,8 +1,14 @@
 // eslint-disable-next-line import/prefer-default-export
 export const normalizeDialog = (dialog) => {
   const newDialog = [];
+  // eslint-disable-next-line no-debugger
+  // debugger;
 
   dialog.forEach((item, i) => {
+    //  i === 0
+    //  fits object is modifying and push in new array
+    //  item.is !== dialog[i - 1].is - different senders
+    //  or current 'is' and previous 'is' not equal (that means senders are different)
     if (i === 0 || item.is !== dialog[i - 1].is) {
       newDialog.push({
         type: 'message',
@@ -20,17 +26,14 @@ export const normalizeDialog = (dialog) => {
         ],
       });
     } else {
+      // this means that message from the same user
       const position = newDialog.length - 1;
-
-      newDialog[position] = {
-        ...newDialog[position],
-        messages: newDialog[position].messages.concat({
-          text: item.message,
-          status: item.status,
-          id: item.id,
-          date: item.date,
-        }),
-      };
+      newDialog[position].messages?.push({
+        text: item.message,
+        status: item.status,
+        id: item.id,
+        date: item.date,
+      });
     }
   });
 
