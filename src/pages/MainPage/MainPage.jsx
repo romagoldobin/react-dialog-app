d13a61d2417323d9a1f14a2c61e3b8b1cb9050ff
romagoldobin <goldobinforwork@gmail.com>
2021-09-03 16:49:23 +0500

    sidebar markup

--- src/pages/MainPage/MainPage.jsx
+++ src/pages/MainPage/MainPage.jsx
@@ -3,6 +3,9 @@ import Chat from '../DialogPage/Chat';
 
 const MainPage = () => (
   <div className="Container">
+    <aside className="Aside">
+      <header>Dialogs</header>
+    </aside>
     <Chat />
   </div>
 );
